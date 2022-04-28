c92e19b2d03f95bf0d37ddf93563a78f03b2e4d1
Tristan Daniël Maat <tm@tlater.net>
2022-04-28 03:42:16 +0100

    flake: Support only x86_64-linux
    I've realized while testing that steamcmd actually only supports
    x86_64, so trying to support more than that makes no sense.
    
    Also no regard for darwin or cygwin, because this is a flake revolving
    around NixOS modules, which will only run on Linux anyway.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,46 @@
 {
   description = "Manage steam game servers with nix!";
 
+  # Only used for testing/dev shells
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    alejandra = {
+      url = "github:kamadorueda/alejandra/1.2.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
+    alejandra,
     nixpkgs,
-    flake-utils,
     ...
   }: let
-    inherit (flake-utils.lib) eachSystem defaultSystems;
-  in
-    {
-      nixosModules.default = import ./modules;
-    }
-    // (eachSystem defaultSystems) (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rnix-lsp
-        ];
-        shellHook = ''
+    # steamcmd only supports x86_64-linux (and in theory Windows, but
+    # this flake is only concerned with NixOS), and the game servers
+    # themselves likely do the same.
+    #
+    # It may be possible to run this stuff with something like 86box
+    # or something like that, but if you're doing anything like that
+    # you're on your own.
+    #
+    # Do share if you're successful though!
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosModules.default = import ./modules;
 
-        '';
-      };
-    });
+    checks.${system} = import ./checks {inherit pkgs;};
+
+    # In theory, we should use flake-utils to define this for more
+    # than just x86. In practice, I'm lazy, and I don't think anyone
+    # will be developing on non-x86.
+    #
+    # Tell me if this annoys you.
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        alejandra.packages.${system}.default
+        rnix-lsp
+      ];
+    };
+  };
 }
